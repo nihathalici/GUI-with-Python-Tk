f2c8a58d35d373d917d8230a76011ae571ab6603
Nihat Halici <nihat.halici@gmail.com>
2022-02-12 16:43:48 +0100

    Add files via upload

---
+++ GUI-Toolkits/006-Python-Tk-Entry-one-line-text-entry.py
@@ -0,0 +1,21 @@
+# 006-Python-Tk-Entry-one-line-text-entry.py
+
+import tkinter as tk
+
+app = tk.Tk()
+app.title('Text Entry')
+
+entry = tk.Entry(app)
+entry.focus()
+entry.pack()
+
+def clicked():
+    print(entry.get())
+
+button = tk.Button(app, text='Show', width=25, command=clicked)
+button.pack()
+
+exit_button = tk.Button(app, text='Close', width=25, command=app.destroy)
+exit_button.pack()
+
+app.mainloop()
