4203e8a4ab7bbfc074fe9e00ef2716db610f19c4
Nihat Halici <nihat.halici@gmail.com>
2022-02-16 08:18:40 +0100

    Add files via upload

---
+++ GUI-Toolkits/008-Python-Tk-Checkbox.py
@@ -0,0 +1,26 @@
+# 008-Python-Tk-Checkbox.py
+
+import tkinter as tk
+
+app = tk.Tk()
+app.title('Checkbox')
+
+var1 = tk.BooleanVar()
+cb1 = tk.Checkbutton(app, text='male', variable=var1)
+cb1.pack()
+
+var2 = tk.BooleanVar()
+cb2 = tk.Checkbutton(app, text='female', variable=var2)
+cb2.pack()
+
+def clicked():
+    print(var1.get())
+    print(var2.get())
+
+button = tk.Button(app, text='Show', width=25, command=clicked)
+button.pack()
+
+exit_button = tk.Button(app, text='Close', width=25, command=app.destroy)
+exit_button.pack()
+
+app.mainloop()
